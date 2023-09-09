f040f5660dede090946373fbbb55576499c193c2
romanmaroder <kashik.roman@rambler.ru>
2023-09-09 18:56:53 +0300

    Added bootstrap class

--- backend/config/main.php
+++ backend/config/main.php
@@ -10,7 +10,9 @@ return [
     'id' => 'app-backend',
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
-    'bootstrap' => ['log'],
+    'bootstrap' => [
+        'log',
+        'common\bootstrap\SetUp'],
     'modules' => [],
     'components' => [
         'request' => [
---
+++ common/bootstrap/SetUp.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace bootstrap;
+
+
+use yii\base\Application;
+
+class SetUp implements \yii\base\BootstrapInterface
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function bootstrap($app): void
+    {
+        // TODO: Implement bootstrap() method.
+    }
+}
