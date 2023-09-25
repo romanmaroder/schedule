9df54376a3b57cfd4dedab41722865b8279e2a30
romanmaroder <kashik.roman@rambler.ru>
2023-09-25 19:47:19 +0300

    Connecting Memcached

--- common/config/main.php
+++ common/config/main.php
@@ -7,8 +7,10 @@ return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
-            'class' => \yii\caching\FileCache::class,
-            'cachePath' => '@common/runtime/cache',
+            //'class' => \yii\caching\FileCache::class,
+            //'cachePath' => '@common/runtime/cache',
+            'class' => \yii\caching\MemCache::class,
+            'useMemcached' => true,
         ],
     ],
 ];
