4062afdd8fb23059f31755bbf4ac4cfd5100d62d
romanmaroder <kashik.roman@rambler.ru>
2023-10-17 17:08:47 +0300

    Commenting on unused menu items

--- backend/views/layouts/sidebar.php
+++ backend/views/layouts/sidebar.php
@@ -40,39 +40,39 @@
                         'icon' => 'tachometer-alt',
                         'badge' => '<span class="right badge badge-info">2</span>',
                         'items' => [
-                            ['label' => 'Active Page', 'url' => ['site/index'], 'iconStyle' => 'far'],
-                            ['label' => 'Inactive Page', 'iconStyle' => 'far'],
+                           //['label' => 'Active Page', 'url' => ['site/index'], 'iconStyle' => 'far'],
+                            //['label' => 'Inactive Page', 'iconStyle' => 'far'],
                             ['label' => 'Users', 'icon' => 'user', 'url' => ['/user/index'], 'active' => $this->context->id == 'user/index'],
                         ]
                     ],
-                    ['label' => 'Simple Link', 'icon' => 'th', 'badge' => '<span class="right badge badge-danger">New</span>'],
+                    //['label' => 'Simple Link', 'icon' => 'th', 'badge' => '<span class="right badge badge-danger">New</span>'],
                     ['label' => 'Yii2 PROVIDED', 'header' => true],
                     ['label' => 'Login', 'url' => ['site/login'], 'icon' => 'sign-in-alt', 'visible' => Yii::$app->user->isGuest],
                     ['label' => 'Gii',  'icon' => 'file-code', 'url' => ['/gii'], 'target' => '_blank'],
                     ['label' => 'Debug', 'icon' => 'bug', 'url' => ['/debug'], 'target' => '_blank'],
-                    ['label' => 'MULTI LEVEL EXAMPLE', 'header' => true],
-                    ['label' => 'Level1'],
-                    [
-                        'label' => 'Level1',
-                        'items' => [
-                            ['label' => 'Level2', 'iconStyle' => 'far'],
-                            [
-                                'label' => 'Level2',
-                                'iconStyle' => 'far',
-                                'items' => [
-                                    ['label' => 'Level3', 'iconStyle' => 'far', 'icon' => 'dot-circle'],
-                                    ['label' => 'Level3', 'iconStyle' => 'far', 'icon' => 'dot-circle'],
-                                    ['label' => 'Level3', 'iconStyle' => 'far', 'icon' => 'dot-circle']
-                                ]
-                            ],
-                            ['label' => 'Level2', 'iconStyle' => 'far']
-                        ]
-                    ],
-                    ['label' => 'Level1'],
-                    ['label' => 'LABELS', 'header' => true],
-                    ['label' => 'Important', 'iconStyle' => 'far', 'iconClassAdded' => 'text-danger'],
-                    ['label' => 'Warning', 'iconClass' => 'nav-icon far fa-circle text-warning'],
-                    ['label' => 'Informational', 'iconStyle' => 'far', 'iconClassAdded' => 'text-info'],
+//                    ['label' => 'MULTI LEVEL EXAMPLE', 'header' => true],
+//                    ['label' => 'Level1'],
+//                    [
+//                        'label' => 'Level1',
+//                        'items' => [
+//                            ['label' => 'Level2', 'iconStyle' => 'far'],
+//                            [
+//                                'label' => 'Level2',
+//                                'iconStyle' => 'far',
+//                                'items' => [
+//                                    ['label' => 'Level3', 'iconStyle' => 'far', 'icon' => 'dot-circle'],
+//                                    ['label' => 'Level3', 'iconStyle' => 'far', 'icon' => 'dot-circle'],
+//                                    ['label' => 'Level3', 'iconStyle' => 'far', 'icon' => 'dot-circle']
+//                                ]
+//                            ],
+//                            ['label' => 'Level2', 'iconStyle' => 'far']
+//                        ]
+//                    ],
+//                    ['label' => 'Level1'],
+//                    ['label' => 'LABELS', 'header' => true],
+//                    ['label' => 'Important', 'iconStyle' => 'far', 'iconClassAdded' => 'text-danger'],
+//                    ['label' => 'Warning', 'iconClass' => 'nav-icon far fa-circle text-warning'],
+//                    ['label' => 'Informational', 'iconStyle' => 'far', 'iconClassAdded' => 'text-info'],
                 ],
             ]);
             ?>
