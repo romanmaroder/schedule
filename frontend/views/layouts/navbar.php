a1d66eacc912bc15dfb32ac43b9d9e301cb5c8c0
romanmaroder <kashik.roman@rambler.ru>
2023-11-14 19:45:58 +0300

    Changed navbar frontend

--- frontend/views/layouts/navbar.php
+++ frontend/views/layouts/navbar.php
@@ -11,54 +11,71 @@ use yii\helpers\Html;
             <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
-            <a href="<?=\yii\helpers\Url::home()?>" class="nav-link">Home</a>
+            <a href="<?= \yii\helpers\Url::home() ?>" class="nav-link">Home</a>
         </li>
-        <li class="nav-item d-none d-sm-inline-block">
-            <a href="#" class="nav-link">Contact</a>
-        </li>
+        <?
+        if (!Yii::$app->user->isGuest): ?>
+            <?
+            echo Html::tag(
+                'li',
+                Html::a(
+                    'Cabinet',
+                    ['/cabinet/default/index'],
+                    ['class' => ['nav-link']]
+                ),
+                ['class' => ['nav-item d-none d-sm-inline-block']]
+            ); ?>
+        <?php
+        endif; ?>
         <li class="nav-item dropdown">
-            <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">Dropdown</a>
+            <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
+               class="nav-link dropdown-toggle">Dropdown</a>
             <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
-                <li><a href="#" class="dropdown-item">Some action </a></li>
-                <li><a href="#" class="dropdown-item">Some other action</a></li>
-                <? if (!Yii::$app->user->isGuest):?>
-                <? echo Html::tag('li', Html::a('Cabinet', ['/cabinet/default/index'], ['class' => ['btn btn-link login text-decoration-none dropdown-item']]), ['class' => ['d-flex']]);
-            echo Html::beginForm(['/auth/auth/logout'], 'post', ['class' => 'd-flex'])
-                . Html::submitButton(
-                    'Logout (' . Yii::$app->user->identity->username . ')',
-                    ['class' => 'btn btn-link logout text-decoration-none dropdown-item']
-                )
-                . Html::endForm()
-                ?>
-                <?php endif;?>
+                <!--<li><a href="#" class="dropdown-item">Some action </a></li>
+                <li><a href="#" class="dropdown-item">Some other action</a></li>-->
+                <?
+                if (!Yii::$app->user->isGuest): ?>
+                    <?
+                    echo Html::tag(
+                        'li',
+                        Html::beginForm(['/auth/auth/logout'], 'post', ['class' => ''])
+                        . Html::submitButton(
+                            'Logout (' . Yii::$app->user->identity->username . ')',
+                            ['class' => 'btn btn-link logout text-decoration-none dropdown-item']
+                        )
+                        . Html::endForm(),
+                    );
+                    ?>
+                <?php
+                endif; ?>
 
-                <li class="dropdown-divider"></li>
+                <!--                <li class="dropdown-divider"></li>-->
 
                 <!-- Level two dropdown-->
-                <li class="dropdown-submenu dropdown-hover">
+                <!--<li class="dropdown-submenu dropdown-hover">
                     <a id="dropdownSubMenu2" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-item dropdown-toggle">Hover for action</a>
                     <ul aria-labelledby="dropdownSubMenu2" class="dropdown-menu border-0 shadow">
                         <li>
                             <a tabindex="-1" href="#" class="dropdown-item">level 2</a>
-                        </li>
+                        </li>-->
 
-                        <!-- Level three dropdown-->
-                        <li class="dropdown-submenu">
-                            <a id="dropdownSubMenu3" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-item dropdown-toggle">level 2</a>
-                            <ul aria-labelledby="dropdownSubMenu3" class="dropdown-menu border-0 shadow">
-                                <li><a href="#" class="dropdown-item">3rd level</a></li>
-                                <li><a href="#" class="dropdown-item">3rd level</a></li>
-                            </ul>
-                        </li>
-                        <!-- End Level three -->
-
-                        <li><a href="#" class="dropdown-item">level 2</a></li>
-                        <li><a href="#" class="dropdown-item">level 2</a></li>
+                <!-- Level three dropdown-->
+                <!--<li class="dropdown-submenu">
+                    <a id="dropdownSubMenu3" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-item dropdown-toggle">level 2</a>
+                    <ul aria-labelledby="dropdownSubMenu3" class="dropdown-menu border-0 shadow">
+                        <li><a href="#" class="dropdown-item">3rd level</a></li>
+                        <li><a href="#" class="dropdown-item">3rd level</a></li>
                     </ul>
-                </li>
-                <!-- End Level two -->
+                </li>-->
+                <!-- End Level three -->
+
+                <!--<li><a href="#" class="dropdown-item">level 2</a></li>
+                <li><a href="#" class="dropdown-item">level 2</a></li>-->
             </ul>
         </li>
+        <!-- End Level two -->
+    </ul>
+    </li>
     </ul>
 
     <!-- SEARCH FORM -->
@@ -76,7 +93,7 @@ use yii\helpers\Html;
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
         <!-- Navbar Search -->
-        <li class="nav-item">
+        <!--<li class="nav-item">
             <a class="nav-link" data-widget="navbar-search" href="#" role="button">
                 <i class="fas fa-search"></i>
             </a>
@@ -95,19 +112,20 @@ use yii\helpers\Html;
                     </div>
                 </form>
             </div>
-        </li>
+        </li>-->
 
         <!-- Messages Dropdown Menu -->
-        <li class="nav-item dropdown">
+        <!--<li class="nav-item dropdown">
             <a class="nav-link" data-toggle="dropdown" href="#">
                 <i class="far fa-comments"></i>
                 <span class="badge badge-danger navbar-badge">3</span>
             </a>
             <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                <a href="#" class="dropdown-item">
-                    <!-- Message Start -->
-                    <div class="media">
-                        <img src="<?=$assetDir?>/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
+                <a href="#" class="dropdown-item">-->
+        <!-- Message Start -->
+        <!--<div class="media">
+                        <img src="<?
+        /*=$assetDir*/ ?>/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
                         <div class="media-body">
                             <h3 class="dropdown-item-title">
                                 Brad Diesel
@@ -116,14 +134,15 @@ use yii\helpers\Html;
                             <p class="text-sm">Call me whenever you can...</p>
                             <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
                         </div>
-                    </div>
-                    <!-- Message End -->
-                </a>
-                <div class="dropdown-divider"></div>
-                <a href="#" class="dropdown-item">
-                    <!-- Message Start -->
-                    <div class="media">
-                        <img src="<?=$assetDir?>/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+                    </div>-->
+        <!-- Message End -->
+        <!--</a>
+        <div class="dropdown-divider"></div>
+        <a href="#" class="dropdown-item">-->
+        <!-- Message Start -->
+        <!-- <div class="media">
+                        <img src="<?
+        /*=$assetDir*/ ?>/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
                         <div class="media-body">
                             <h3 class="dropdown-item-title">
                                 John Pierce
@@ -132,14 +151,15 @@ use yii\helpers\Html;
                             <p class="text-sm">I got your message bro</p>
                             <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
                         </div>
-                    </div>
-                    <!-- Message End -->
-                </a>
-                <div class="dropdown-divider"></div>
-                <a href="#" class="dropdown-item">
-                    <!-- Message Start -->
-                    <div class="media">
-                        <img src="<?=$assetDir?>/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+                    </div>-->
+        <!-- Message End -->
+        <!--</a>
+        <div class="dropdown-divider"></div>
+        <a href="#" class="dropdown-item">-->
+        <!-- Message Start -->
+        <!--<div class="media">
+                        <img src="<?
+        /*=$assetDir*/ ?>/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
                         <div class="media-body">
                             <h3 class="dropdown-item-title">
                                 Nora Silvester
@@ -148,15 +168,15 @@ use yii\helpers\Html;
                             <p class="text-sm">The subject goes here</p>
                             <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
                         </div>
-                    </div>
-                    <!-- Message End -->
-                </a>
-                <div class="dropdown-divider"></div>
-                <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
-            </div>
-        </li>
+                    </div>-->
+        <!-- Message End -->
+        <!--</a>
+        <div class="dropdown-divider"></div>
+        <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
+    </div>-->
+        <!--</li>-->
         <!-- Notifications Dropdown Menu -->
-        <li class="nav-item dropdown">
+        <!--<li class="nav-item dropdown">
             <a class="nav-link" data-toggle="dropdown" href="#">
                 <i class="far fa-bell"></i>
                 <span class="badge badge-warning navbar-badge">15</span>
@@ -181,7 +201,7 @@ use yii\helpers\Html;
                 <div class="dropdown-divider"></div>
                 <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
             </div>
-        </li>
+        </li>-->
         <li class="nav-item">
             <?= Html::a('<i class="fas fa-sign-out-alt"></i>', ['/auth/auth/logout'], ['data-method' => 'post', 'class' => 'nav-link']) ?>
         </li>
