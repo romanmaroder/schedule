666cec4dde69162a8de654917e551aa08278f10d
romanmaroder <kashik.roman@rambler.ru>
2024-01-20 20:05:52 +0300

    Search user by role during authorization

--- backend/views/auth/login.php
+++ backend/views/auth/login.php
@@ -1,66 +1,74 @@
 <?php
 
-use yii\bootstrap4\ActiveForm;
 use yii\helpers\Html;
 
 /* @var $model \schedule\forms\auth\LoginForm */
 ?>
-        <div class="card">
-            <div class="card-body login-card-body">
-                <p class="login-box-msg">Sign in to start your session</p>
+<div class="">
+    <?php
+    if (Yii::$app->getSession()->hasFlash('success')): ?>
+        <?php
+        echo '<p class="text-success text-center"><b>' . Yii::$app->session->getFlash('success') . '</b></p>'; ?>
+    <?php
+    else: ?>
+        <?php
+        echo '<p class="text-danger text-center"><b>' . Yii::$app->session->getFlash('error') . '</b></p>'; ?>
+    <?php
+    endif; ?>
+</div>
+<div class="card">
+    <div class="card-body login-card-body">
+        <p class="login-box-msg">Login to start your session</p>
 
-                <?php $form = ActiveForm::begin(['id' => 'login-form']) ?>
+        <?php
+        $form = \yii\bootstrap5\ActiveForm::begin(['id' => 'login-form']) ?>
 
-                <?= $form->field($model,'username', [
-                    'options' => ['class' => 'form-group has-feedback'],
-                    'template' => '{beginWrapper}{input}{error}{endWrapper}',
-                    'wrapperOptions' => ['class' => 'mb-3']
-                ])
-                    ->label(false)
-                    ->textInput(['placeholder' => $model->getAttributeLabel('username')]) ?>
+        <?= $form->field(
+            $model,
+            'username',
+            [
+                'options' => ['class' => 'form-group has-feedback'],
+                'inputTemplate' => '{input}<div class="input-group-append"><div class="input-group-text"><span class="fas fa-envelope"></span></div></div>',
+                'template' => '{beginWrapper}{input}{error}{hint}{endWrapper}',
+                'wrapperOptions' => ['class' => 'input-group mb-3']
+            ]
+        )
+            ->label(false)
+            ->textInput(['placeholder' => $model->getAttributeLabel('username')]) ?>
 
-                <?= $form->field($model, 'password', [
-                    'options' => ['class' => 'form-group has-feedback'],
-                    'template' => '{beginWrapper}{input}{error}{endWrapper}',
-                    'wrapperOptions' => ['class' => 'mb-3']
-                ])
-                    ->label(false)
-                    ->passwordInput(['placeholder' => $model->getAttributeLabel('password')]) ?>
+        <?= $form->field(
+            $model,
+            'password',
+            [
+                'options' => ['class' => 'form-group has-feedback'],
+                'inputTemplate' => '{input}<div class="input-group-append"><div class="input-group-text"><span class="fas fa-lock"></span></div></div>',
+                'template' => '{beginWrapper}{input}{error}{endWrapper}',
+                'wrapperOptions' => ['class' => 'input-group mb-3']
+            ]
+        )
+            ->label(false)
+            ->passwordInput(['placeholder' => $model->getAttributeLabel('password')]) ?>
 
-                <div class="row">
-                    <!--<div class="col-8">
-                        <?/*= $form->field($model, 'rememberMe')->checkbox([
-                            'template' => '<div class="icheck-primary">{input}{label}</div>',
-                            'labelOptions' => [
-                                'class' => ''
-                            ],
-                            'uncheck' => null
-                        ]) */?>
-                    </div>-->
-                    <div class="col-4">
-                        <?= Html::submitButton('Sign In', ['class' => 'btn btn-primary btn-block']) ?>
-                    </div>
-                </div>
-
-                <?php ActiveForm::end(); ?>
-
-                <!--<div class="social-auth-links text-center mb-3">
-                    <p>- OR -</p>
-                    <a href="#" class="btn btn-block btn-primary">
-                        <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-                    </a>
-                    <a href="#" class="btn btn-block btn-danger">
-                        <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-                    </a>
-                </div>-->
-                <!-- /.social-auth-links -->
-
-                <!--<p class="mb-1">
-                    <a href="forgot-password.html">I forgot my password</a>
-                </p>
-                <p class="mb-0">
-                    <a href="register.html" class="text-center">Register a new membership</a>
-                </p>-->
+        <div class="row">
+           <!-- <div class="col-8">
+                <?/*= $form->field($model, 'rememberMe')->checkbox(
+                    [
+                        'template' => '<div class="icheck-primary">{input}{label}</div>',
+                        'labelOptions' => [
+                            'class' => ''
+                        ],
+                        'uncheck' => null
+                    ]
+                ) */?>
+            </div>-->
+            <div class="col-4">
+                <?= Html::submitButton('Sign In', ['class' => 'btn btn-primary btn-block']) ?>
             </div>
-            <!-- /.login-card-body -->
         </div>
+
+        <?php
+        \yii\bootstrap5\ActiveForm::end(); ?>
+
+    </div>
+    <!-- /.login-card-body -->
+</div>
--- schedule/repositories/UserRepository.php
+++ schedule/repositories/UserRepository.php
@@ -15,13 +15,54 @@ class UserRepository
      */
     public function findByUsernameOrEmail($value): ?User
     {
-        return User::find()
-            ->alias('u')
-            ->andWhere(['exists', Employee::find()
-                ->alias('e')
-                ->andWhere('u.id = e.user_id')
-                ->andWhere(['u.username' => $value])]
-            )->one();
+        switch (\Yii::$app->id) {
+            case 'app-backend':
+                return User::find()
+                    ->alias('u')
+                    ->andWhere(
+                        [
+                            'exists',
+                            Employee::find()->joinWith('role')
+                                ->alias('e')
+                                ->andWhere('u.id = e.user_id')
+                                ->andWhere(['u.username' => $value])
+                                ->andWhere(
+                                    ['or', ['schedule_roles.id' => '1'], ['schedule_roles.id' => '2']] //TODO think about defining mandatory roles for the admin panel
+                                )
+                        ]
+                    )->one();
+
+            case 'app-frontend':
+                return User::find()
+                    ->alias('u')
+                    ->andWhere(
+                        [
+                            'exists',
+                            Employee::find()
+                                ->alias('e')
+                                ->andWhere('u.id = e.user_id')
+                                ->andWhere(['u.username' => $value])
+                        ]
+                    )->one();
+        }
+        /*$app = \Yii::$app->id;
+        return match ($app) {
+            'app-backend' => User::find()
+                ->alias('u')
+                ->andWhere(['exists', Employee::find()->joinWith('role')
+                               ->alias('e')
+                               ->andWhere('u.id = e.user_id')
+                               ->andWhere(['u.username' => $value])
+                              ->andWhere(['or',['schedule_roles.name'=>'admin'],['schedule_roles.name'=>'master']])]
+                )->one(),
+            'app-frontend' => User::find()
+                ->alias('u')
+                ->andWhere(['exists', Employee::find()
+                               ->alias('e')
+                               ->andWhere('u.id = e.user_id')
+                               ->andWhere(['u.username' => $value])]
+                )->one(),
+        };*/
     }
 
     /**
