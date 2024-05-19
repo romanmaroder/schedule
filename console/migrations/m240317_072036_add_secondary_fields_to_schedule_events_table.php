ed35fa475ec5aa13c00fe77860c84b6abc7f2baf
romanmaroder <kashik.roman@rambler.ru>
2024-05-19 18:03:21 +0300

    Added auxiliary fields for record table

--- console/migrations/m240317_072036_add_secondary_fields_to_schedule_events_table.php
+++ console/migrations/m240317_072036_add_secondary_fields_to_schedule_events_table.php
@@ -3,9 +3,9 @@
 use yii\db\Migration;
 
 /**
- * Class m240317_072036_add_discounts_amount_status_fields_to_schedule_events_table
+ * Class m240317_072036_add_secondary_fields_to_schedule_events_table
  */
-class m240317_072036_add_discounts_amount_status_fields_to_schedule_events_table extends Migration
+class m240317_072036_add_secondary_fields_to_schedule_events_table extends Migration
 {
 
     /**
@@ -17,6 +17,10 @@ class m240317_072036_add_discounts_amount_status_fields_to_schedule_events_table
         $this->addColumn('{{%schedule_events}}', 'discount_from', $this->smallInteger()->after('discount'));
         $this->addColumn('{{%schedule_events}}', 'amount', $this->integer()->after('discount_from'));
         $this->addColumn('{{%schedule_events}}', 'status', $this->smallInteger()->after('amount'));
+        $this->addColumn('{{%schedule_events}}', 'rate', $this->integer()->after('status'));
+        $this->addColumn('{{%schedule_events}}', 'price', $this->integer()->after('rate'));
+        $this->addColumn('{{%schedule_events}}', 'fullname', $this->string()->after('price'));
+        $this->addColumn('{{%schedule_events}}', 'default_color', $this->string()->defaultValue('#747d8c')->after('fullname'));
     }
 
     /**
@@ -28,5 +32,9 @@ class m240317_072036_add_discounts_amount_status_fields_to_schedule_events_table
         $this->dropColumn('{{%schedule_events}}', 'discount_from');
         $this->dropColumn('{{%schedule_events}}', 'amount');
         $this->dropColumn('{{%schedule_events}}', 'status');
+        $this->dropColumn('{{%schedule_events}}', 'rate');
+        $this->dropColumn('{{%schedule_events}}', 'price');
+        $this->dropColumn('{{%schedule_events}}', 'fullname');
+        $this->dropColumn('{{%schedule_events}}', 'default_color');
     }
 }
