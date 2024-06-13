9ed75738c6dee8720cc69535253a2fbcba225e26
romanmaroder <kashik.roman@rambler.ru>
2024-06-13 12:22:05 +0300

    Added Swagger annotations

--- api/controllers/SiteController.php
+++ api/controllers/SiteController.php
@@ -4,6 +4,45 @@ namespace api\controllers;
 
 use yii\rest\Controller;
 
+/**
+ * @SWG\Swagger(
+ *     basePath="/",
+ *     host="api.schedule.dev",
+ *     schemes={"http"},
+ *     produces={"application/json","application/xml"},
+ *     consumes={"application/json","application/xml","application/x-www-form-urlencoded"},
+ *     @SWG\Info(
+ *         version="1.0.0",
+ *         title="Shop API",
+ *         description="HTTP JSON API",
+ *     ),
+ *     @SWG\SecurityScheme(
+ *         securityDefinition="OAuth2",
+ *         type="oauth2",
+ *         flow="password",
+ *         tokenUrl="http://api.schedule.dev/oauth2/token"
+ *     ),
+ *     @SWG\SecurityScheme(
+ *         securityDefinition="Bearer",
+ *         type="apiKey",
+ *         name="Authorization",
+ *         in="header"
+ *     ),
+ *     @SWG\Definition(
+ *         definition="ErrorModel",
+ *         type="object",
+ *         required={"code", "message"},
+ *         @SWG\Property(
+ *             property="code",
+ *             type="integer",
+ *         ),
+ *         @SWG\Property(
+ *             property="message",
+ *             type="string"
+ *         )
+ *     )
+ * )
+ */
 class SiteController extends Controller
 {
     public function actionIndex(): array
