906a4e02713076420ba5a814ba284fae6a2014ac
romanmaroder <kashik.roman@rambler.ru>
2024-10-08 11:15:58 +0300

    Adding fields to the events table

---
+++ console/migrations/m241008_074232_add_created_at_and_updated_at_to_schedule_events_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m241008_074232_add_created_at_and_updated_at_to_schedule_events_table
+ */
+class m241008_074232_add_created_at_and_updated_at_to_schedule_events_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function up()
+    {
+        $this->addColumn('{{%schedule_events}}', 'created_at', $this->integer()->unsigned()->notNull());
+        $this->addColumn('{{%schedule_events}}', 'updated_at', $this->integer()->unsigned()->notNull());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function down()
+    {
+        $this->dropColumn('{{%schedule_events}}', 'created_at');
+        $this->dropColumn('{{%schedule_events}}', 'updated_at');
+    }
+
+}
